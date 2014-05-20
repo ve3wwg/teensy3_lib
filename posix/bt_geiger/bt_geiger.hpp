07c405b43a7d95ee1521f651d189f7efd6c3b3fb
Warren Gay <ve3wwg@gmail.com>
2014-05-19 21:57:49 -0400

    Added packed attribute

--- posix/bt_geiger/bt_geiger.hpp
+++ posix/bt_geiger/bt_geiger.hpp
@@ -6,8 +6,6 @@
 #ifndef BT_GEIGER_HPP
 #define BT_GEIGER_HPP
 
-#include "sliptty.hpp"
-
 #define UNKNOWN_BT 		"/dev/cu.Bluetooth"
 #define KEY_BLUETOOTH_DEV	"bluetooth_dev"
 #define KEY_BLUETOOTH_SPEED	"bluetooth_speed"
@@ -16,22 +14,21 @@ enum PacketType {
 	PT_CurrentTime = 7,		// s_current_time
 };
 
-struct s_packet {
-	PacketType	pkttype;	// Packet type
-};
-
 //////////////////////////////////////////////////////////////////////
 // Used to inform Teensy what the current date/time is
 //////////////////////////////////////////////////////////////////////
 
-struct s_current_time : public s_packet {
+#define SZ_CURRENT_TIME		1+6
+
+struct s_current_time {
+	uint8_t		pkttype;
 	uint8_t		year;		// Years since 2014
 	uint8_t		month;		// Month 1-12
 	uint8_t		mday;		// Day of month 1-31
 	uint8_t		hour;		// 0-23
 	uint8_t		min;		// 0-59
 	uint8_t		sec;		// 0-59
-};
+} __attribute__((packed));
 
 //////////////////////////////////////////////////////////////////////
 // Union of all supported packet types
@@ -40,9 +37,7 @@ struct s_current_time : public s_packet {
 union u_packets {
 	uint8_t		pkt_type;
 	s_current_time	pkt_curtime;
-};
-
-extern SlipTty bluetooth;
+} __attribute__((packed));
 
 #endif // BT_GEIGER_HPP
 
