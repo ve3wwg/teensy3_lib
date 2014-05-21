615ffe9d0d2c49d197b1bffe6b360a7d4b9e1c95
Warren Gay <ve3wwg@gmail.com>
2014-05-20 21:49:04 -0400

    Now runs with fibers, rx and tx loops

--- teensy3/projects/bt_geiger/bt_geiger.cpp
+++ teensy3/projects/bt_geiger/bt_geiger.cpp
@@ -5,46 +5,75 @@
 
 #include <Arduino.h>
 #include <stdint.h>
+#include <fibers.h>
 
 #define TEENSY
 #include "bt_geiger.hpp"
 #include "slipser.hpp"
 
+static Fibers<4> fibers(4096);
 static SlipSer bluetooth(Serial2);
-
+static int led = 13;
 static char rxbuf[1024];
 
 static void
 send_current_time() {
-	static uint8_t sec = 0;
+	unsigned long t = Teensy3Clock.get();
 	s_current_time msg;
 
 	msg.pkttype = PT_CurrentTime;
-	msg.year = 0;
-	msg.month = 5;
-	msg.mday = 19;
-	msg.hour = 17;
-	msg.min = 0;
-	msg.sec = sec++;
-
+	msg.time = static_cast<uint32_t>(t);
 	bluetooth.write(&msg,sizeof msg);
+}
+
+static void
+rx_thread(void *) {
+	u_packets *packet = 0;
+	unsigned pktlen = 0;
+	int led_state = 1;
+	
+	for (;;) {
+		packet = static_cast<u_packets *>(bluetooth.read(pktlen));
+		led_state ^= 1;
+		digitalWrite(led,led_state ? HIGH : LOW);
+	
+		switch ( packet->pkt_type ) {
+		case PT_CurrentTime :
+			if ( pktlen == SZ_CURRENT_TIME ) {
+				time_t rtc = Teensy3Clock.get();
+				time_t mac = packet->pkt_curtime.time;
+			
+				if ( rtc != mac )
+					Teensy3Clock.set(mac);		// Adjust our realtime clock
+			}
+			break;
+		default :
+			;
+		}
+		yield();
+	}
+}
+
+extern "C" {
+	void yield() {
+		fibers.yield();
+	}
 }
 
 int
 main() {
-	int led = 13;
 
 	pinMode(led,OUTPUT);
 	digitalWrite(led,HIGH);
 
 	bluetooth.open(38400,rxbuf,sizeof rxbuf);
 
-	int x = 0;
+	fibers.create(rx_thread,0,4096);
 
 	for (;;) {
-		digitalWrite(led,x?LOW:HIGH);
 		send_current_time();
 		delay(1000);
+		fibers.yield();
 	}
 	
 	return 0;
